0e7bce8e4e3b0a79e3fb6c603fcb5fd06c419894
alan bount <alan@zeroasterisk.com>
2018-12-11 21:56:33 -0700

    WIP roomy worker basics

--- README.md
+++ README.md
@@ -1,21 +1,54 @@
 # Roomy
 
-**TODO: Add description**
+A simple example of OTP process state, and
+[kubernetes](https://kubernetes.io)
+(or other) cluster managed by
+[libcluster](https://github.com/bitwalker/libcluster)
+and
+[swarm](https://github.com/bitwalker/swarm).
 
-## Installation
+## Basics of our GenServer
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `roomy` to your list of dependencies in `mix.exs`:
+OTP FTW!
+
+In this case, we will use Swarm Supervisors to start processes to manage rooms, one worker process for each room.
 
 ```elixir
-def deps do
-  [
-    {:roomy, "~> 0.1.0"}
-  ]
-end
+{:ok, pid} = Roomy.add_room(:bathroom_1)
+{:ok, pid} = Roomy.add_room(:living_room)
+{:ok, [{pid, :bathroom_1}, {pid, :living_room}]} = Roomy.list_rooms()
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/roomy](https://hexdocs.pm/roomy).
+And we can have people *walk into* rooms.
 
+```elixir
+{:ok, pid} = Roomy.walk_into(:bathroom_1, :alan)
+{:ok, pid} = Roomy.walk_into(:living_room, :james)
+{:ok, pid} = Roomy.walk_into(:living_room, :alan)
+{:ok, [:alan, :james]} = Roomy.who_is_in(:living_room)
+{:ok, []} = Roomy.who_is_in(:bathroom_1)
+{:ok, %{bathroom_1: [], living_room: [:alan, :james]}} = Roomy.house_map()
+:TODO = Roomy.print_house()
+```
+
+Assumptions:
+* All rooms and people are represented by simple atoms.
+* All rooms and people are unique and idempotent (can only exist once).
+* People can only be in one room at a time, walking into a different room removes them from the previous room.
+
+And we can assign people to rooms based on various criteria.
+
+```elixir
+{:ok, pid} = Roomy.assign([:poppy, :anita, :becky], :introvert)      # to least populated
+{:ok, pid} = Roomy.assign([:alan, :oliver, :james], :extrovert)      # to most populated
+:TODO = Roomy.print_house()
+```
+
+## Basics of our Cluster
+
+There is no database in this example.  All state lives in GenServers.
+
+But state must migrate when nodes add/exit the cluster.  That's the responsibility of `libcluster` and `swarm`.
+
+TODO build k8 cluster
+TODO libcluster config
---
+++ lib/roomy/room_worker.ex
@@ -0,0 +1,129 @@
+defmodule Roomy.RoomWorker do
+  use GenServer
+  require Logger
+  @moduledoc """
+  This is the worker process, in this case,
+  It is responsible for a single room in our house.
+  It maintains state on who is in that room.
+  #
+  # can recieve a [room_name] or [room_name, people_in_room]
+  """
+  def start_link(room_name), do: start_link(room_name, [])
+  def start_link(room_name, people_in_room) when is_atom(people_in_room), do: start_link(room_name, [people_in_room])
+  def start_link(room_name, people_in_room) when is_list(people_in_room) do
+    GenServer.start_link(__MODULE__, [room_name, people_in_room], [
+      # used for name registration for GenServer
+      name: room_name,
+      timeout: 10_000
+    ])
+  end
+
+  @doc """
+  Stop
+  """
+  def stop(pid, reason \\ :normal), do: GenServer.stop(pid, reason)
+
+  @doc """
+  Who is in this room (list)
+  """
+  def who_is_in(pid), do: GenServer.call(pid, {:who_is_in})
+
+  @doc """
+  Add a person to this room
+  """
+  def add_person(pid, person), do: GenServer.call(pid, {:add_person, person})
+
+  @doc """
+  Remove a person from this room
+  """
+  def rm_person(pid, person), do: GenServer.call(pid, {:rm_person, person})
+
+
+  # initialize the GenServer to maintain the state of the application
+  #
+  # can recieve a [room_name] or [room_name, people_in_room]
+  def init([room_name, people_in_room]) when is_list(people_in_room) do
+    {:ok, {room_name, MapSet.new(people_in_room)}}
+  end
+  # def init([room_name]) do
+  #   {:ok, {room_name, []}}
+  # end
+
+  # who is in this room right now? return a simple list of people
+  def handle_call({:who_is_in}, _from, {_room_name, people_in_room} = state) do
+    {:reply, {:ok, people_in_room |> MapSet.to_list()}, state}
+  end
+
+  # we add a person to this room (not in scope, removal from other rooms)
+  def handle_call({:add_person, people}, _from, {room_name, people_in_room}) when is_list(people) do
+    people_in_room = people_in_room |> MapSet.union(MapSet.new(people))
+    {:reply, :ok, {room_name, people_in_room}}
+  end
+  def handle_call({:add_person, person}, _from, {room_name, people_in_room}) do
+    people_in_room = people_in_room |> MapSet.put(person)
+    {:reply, :ok, {room_name, people_in_room}}
+  end
+
+  # we remove a person from this room
+  def handle_call({:rm_person, people}, _from, {room_name, people_in_room}) when is_list(people) do
+  def handle_call({:rm_person, person}, _from, {room_name, people_in_room}) do
+    people_in_room = people_in_room |> MapSet.delete(person)
+    {:reply, :ok, {room_name, people_in_room}}
+  end
+
+  # called when a handoff has been initiated due to changes
+  # in cluster topology, valid response values are:
+  #
+  #   - `:restart`, to simply restart the process on the new node
+  #   - `{:resume, state}`, to hand off some state to the new process
+  #   - `:ignore`, to leave the process running on its current node
+  #
+  def handle_call({:swarm, :begin_handoff}, _from, {room_name, people_in_room}) do
+    {:reply, {:resume, {room_name, people_in_room}}, {room_name, people_in_room}}
+  end
+  # called after the process has been restarted on its new node,
+  # and the old process' state is being handed off. This is only
+  # sent if the return to `begin_handoff` was `{:resume, state}`.
+  # **NOTE**: This is called *after* the process is successfully started,
+  # so make sure to design your processes around this caveat if you
+  # wish to hand off state like this.
+  def handle_cast({:swarm, :end_handoff, people_in_room}, {room_name, _}) do
+    {:noreply, {room_name, people_in_room}}
+  end
+  # called when a network split is healed and the local process
+  # should continue running, but a duplicate process on the other
+  # side of the split is handing off its state to us. You can choose
+  # to ignore the handoff state, or apply your own conflict resolution
+  # strategy
+  def handle_cast({:swarm, :resolve_conflict, other_node_state}, {room_name, people_in_room} = state) do
+    Logger.debug("Room: #{room_name} contains: #{inspect(people_in_room)} got a resolve_conflict message from other node #{inspect(other_node_state)}")
+    {:noreply, state}
+  end
+
+  # this message is sent when this process should die
+  # because it is being moved, use this as an opportunity
+  # to clean up
+  def handle_info({:swarm, :die}, {room_name, people_in_room} = state) do
+    # should do cleanup...?  maybe swarm will auto-move?
+    Logger.debug("Room: #{room_name} contains: #{inspect(people_in_room)} got :swarm :die")
+    {:stop, :shutdown, state}
+  end
+  def handle_info(:timeout, {room_name, people_in_room} = state) do
+    Logger.debug("Room: #{room_name} contains: #{inspect(people_in_room)} got :timeout")
+    {:stop, :shutdown, state}
+  end
+
+  # we can also remove as a cast, in the background/async
+  def handle_cast(:rm_person, person, {room_name, people_in_room}) do
+    people_in_room = people_in_room |> MapSet.delete(person)
+    {:noreply, {room_name, people_in_room}}
+  end
+
+  # we don't really need a loop, but sometimes it's useful to see...
+  # def handle_info(:loop, {room_name, people_in_room}) do
+  #   Logger.debug("Room: #{room_name} contains: #{inspect(people_in_room)}")
+  #   Process.send_after(self(), :loop, people_in_room)
+  #   {:noreply, {room_name, people_in_room}}
+  # end
+
+end
